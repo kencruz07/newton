9c5e7634e2dc02531404bfbc5cefd29c57232cb9
Kenneth Cruz <kencruz07@gmail.com>
2016-02-23 19:07:47 +0800

    Rework Newton class using new specifications * Create Newton.Element  * Uses methods of current existing ElementBuilder class  * Create render function which returns appendable (Node, HTMLElement) object depending on Newton.Element type and attrs

--- js/client.js
+++ js/client.js
@@ -1,32 +1,44 @@
-var TextComponent = Newton.createClass({
-  render: function(){
-    return $('p', {className: 'text-component'}, this.props.text);
-  }
-});
+// var TextComponent = Newton.createClass({
+//   render: function(){
+//     return $('p', {className: 'text-component'}, this.props.text);
+//   }
+// });
 
-var BoxComponent = Newton.createClass({
-  render: function(){
-    return $('div', {className: 'box'});
-  }
-});
+// var BoxComponent = Newton.createClass({
+//   render: function(){
+//     return $('div', {className: 'box'});
+//   }
+// });
 
-var hello = new TextComponent({text: 'hello'}),
-    world = new TextComponent({text: 'world'}),
-    box = new BoxComponent();
+// var hello = new TextComponent({text: 'hello'}),
+//     world = new TextComponent({text: 'world'}),
+//     box = new BoxComponent();
 
-document.body.appendChild(hello.getElement());
-document.body.appendChild(world.getElement());
-document.body.appendChild(box.getElement());
+// document.body.appendChild(hello.getElement());
+// document.body.appendChild(world.getElement());
+// document.body.appendChild(box.getElement());
 
-console.log(hello);
-console.log(world);
-console.log(box);
-console.log(hello instanceof TextComponent);
-console.log(hello instanceof Newton.Component);
-console.log(hello.__proto__);
+// console.log(hello);
+// console.log(world);
+// console.log(box);
+// console.log(hello instanceof TextComponent);
+// console.log(hello instanceof Newton.Component);
+// console.log(hello.__proto__);
 
-console.log(hello.getUID()); // 0
-console.log(world.getUID()); // 1
-console.log(box.getUID()); // 2
+// console.log(hello.getUID()); // 0
+// console.log(world.getUID()); // 1
+// console.log(box.getUID()); // 2
+
+// console.log(document.body);
+
+var foo = new Newton.Element('div', {className: 'foo'});
+// console.log(foo);
+
+var baz = new Newton.Element('span', {className: 'baz'});
+// console.log(baz);
+
+foo.setChildren([baz]);
+
+document.body.appendChild(foo.render());
 
 console.log(document.body);
--- js/newton.js
+++ js/newton.js
@@ -1,32 +1,71 @@
-function Newton(){
-  this.UID = 0;
+// function Newton(){
+//   this.UID = 0;
+// }
+
+// function Component(params){
+//   this.params = params;
+//   this.UID = Newton.UID;
+//   this.renderObject = Newton.renderObject;
+//   Newton.UID++;
+//   return this;
+// }
+
+// Component.prototype.getElement = function(){
+//   // this.renderObject = Newton.renderObject; // ????
+//   this.renderObject.props = this.params;
+//   var something = this.renderObject.render();
+//   something.setAttribute('data-newton-id', this.UID);
+//   return something;
+// }
+
+// Component.prototype.getUID = function(){
+//   return this.UID;
+// }
+
+// Newton.prototype.Component = Component;
+
+// Newton.prototype.createClass = function(renderObject){
+//   this.renderObject = renderObject;
+//   return this.Component;
+// }
+
+function Newton(){}
+
+function Element(type, attrs){
+  this.type = type;
+  this.attrs = attrs;
 }
 
-function Component(params){
-  this.params = params;
-  return this;
+Element.prototype.render = function(){
+  this.element = document.createElement(this.type);
+
+  this._buildAttributes(this.attrs);
+  if (this.children) this._appendChildren(this.children);
+
+  return this.element;
 }
 
-Component.prototype.getElement = function(){
-  this.renderObject = Newton.renderObject;
-  this.UID = Newton.UID;
-  this.renderObject.props = this.params;
-  var something = this.renderObject.render();
-  something.setAttribute('data-newton-id', this.UID);
-  Newton.UID++;
-  return something;
+Element.prototype.setChildren = function(elements_array){
+  this.children = elements_array;
 }
 
-Component.prototype.getUID = function(){
-  return this.UID;
+Element.prototype._appendChildren = function(children) {
+  for (var i = 0; i < children.length; i++) {
+    // child = (children[i] instanceof HTMLElement) ?
+    //   children[i].cloneNode(true) :
+    //   document.createTextNode(children[i]);
+
+    this.element.appendChild(children[i].render());
+  }
 }
 
-Newton.prototype.Component = Component;
-
-Newton.prototype.createClass = function(renderObject){
-  this.renderObject = renderObject;
-  return Newton.Component;
+Element.prototype._buildAttributes = function(attrs) {
+  for (var attr in attrs) {
+    this.element[attr] = attrs[attr];
+  }
 }
+
+Newton.prototype.Element = Element;
 
 
 window.Newton = new Newton();
